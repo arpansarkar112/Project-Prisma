e55d45c98fa0bb912405715ebf169f62e6d544d7
Arpan Sarkar <arpansarkar112@gmail.com>
2026-07-03 17:34:01 +0300

    project setup

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import cookieParser from "cookie-parser";
+import express, { Application, Request, Response } from "express";
+import cors from "cors"
+import config from "./config";
+import { userRoutes } from "./modules/user/user.route";
+
+const app: Application = express()
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('/', async (req: Request, res: Response) => {
+    // const user = await prisma.user.findMany()
+    // console.log(user)
+    res.send('Hello World!')
+})
+
+app.use("/api/users", userRoutes)
+
+export default app
